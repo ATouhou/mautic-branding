369789b6c4929b18e4ae3786553423c771d4ee40
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-02-24 10:28:30 -0800

    setup basic file structure

---
+++ Config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array (
+  'name' => 'Mautic Branding',
+  'description' => 'Plugin allows you to modify logo and branding on Mautic',
+  'author' => 'Karen Freeman-Smith',
+  'version' => '0.0.1'
+);
+
+ ?>
---
+++ MauticBrandingBundle.php
@@ -0,0 +1,9 @@
+<?php
+namespace MauticPlugins\MauticBrandingBundle;
+
+use Mautic\PluginBundle\Bundle\PluginBundleBase;
+
+class MauticBrandingBundle extends PluginBundleBase {
+  //
+}
+?>
